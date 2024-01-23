b5f53c70a1696e80b25ae3b8b7dfd96fe51cc06f
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2024-01-23 14:09:41 -0500

    add SignIn component, add routing via nav bar 10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,19 +1,35 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Pressable } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
-import AppBarTab from './AppBarTab';
+import { Link } from 'react-router-native';
+import Text from './Text';
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.darkBackground,
   },
+  navItem: {
+    padding: 15,
+  },
 });
+
+const AppBarTab = ({ to, text }) => {
+  return (
+    <Link to={to} style={styles.navItem}>
+      <Text color="white" fontWeight="bold">
+        {text}
+      </Text>
+    </Link>
+  );
+};
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab text="Repositories"></AppBarTab>
+      <AppBarTab to="/" text="Repositories"></AppBarTab>
+      <AppBarTab to="/sign-in" text="Sign in"></AppBarTab>
     </View>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,7 @@ import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
 import theme from '../theme';
 import { Navigate, Route, Routes } from 'react-router-native';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,6 +19,7 @@ const Main = () => {
       <AppBar />
       <Routes>
         <Route path="/" element={<RepositoryList />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </View>
