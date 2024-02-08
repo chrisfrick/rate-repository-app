5e2a1649410c5c8d859cb3b095e4ae05850862bb
chrisfrick <108237130+chrisfrick@users.noreply.github.com>
2024-02-08 13:02:27 -0500

    implement sorting feature for repository list view 10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,8 +3,107 @@ import { useNavigate } from 'react-router-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import ItemSeparator from './ItemSeparator';
+import {
+  Button,
+  Icon,
+  Menu,
+  Modal,
+  PaperProvider,
+  Portal,
+} from 'react-native-paper';
+import { useState } from 'react';
+import Text from './Text';
 
-export const RepositoryListContainer = ({ repositories }) => {
+const styles = StyleSheet.create({
+  sortStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+    paddingRight: 15,
+    alignItems: 'center',
+    height: 60,
+  },
+  menu: {
+    backgroundColor: 'white',
+    margin: 15,
+  },
+});
+
+const ListSortSelect = ({ setSortOrder, sortOrder }) => {
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
+  let anchorText = '';
+
+  switch (sortOrder) {
+    case 'LATEST':
+      anchorText = 'Latest repositories';
+      break;
+    case 'HIGHEST_RATED':
+      anchorText = 'Highest rated repositories';
+      break;
+    case 'LOWEST_RATED':
+      anchorText = 'Lowest rated repositories';
+      break;
+    default:
+      anchorText = 'Sort by...';
+  }
+
+  return (
+    <View>
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={
+          <Pressable onPress={openMenu} onDismiss={closeMenu}>
+            <View style={styles.sortStyle}>
+              <Text fontSize="subheading">{anchorText}</Text>
+              <Icon source="menu-down" size={20} />
+            </View>
+          </Pressable>
+        }
+      >
+        <Portal>
+          <Modal visible={visible} onDismiss={closeMenu}>
+            <View style={styles.menu}>
+              <Menu.Item title="Select an item..." disabled />
+              <Menu.Item
+                onPress={() => {
+                  setSortOrder('LATEST');
+                  closeMenu();
+                }}
+                title="Latest repositories"
+              />
+              <Menu.Item
+                onPress={() => {
+                  setSortOrder('HIGHEST_RATED');
+                  closeMenu();
+                }}
+                title="Highest rated repositories"
+              />
+              <Menu.Item
+                onPress={() => {
+                  setSortOrder('LOWEST_RATED');
+                  closeMenu();
+                }}
+                title="Lowest rated repositories"
+              />
+            </View>
+          </Modal>
+        </Portal>
+      </Menu>
+    </View>
+  );
+};
+
+export const RepositoryListContainer = ({
+  repositories,
+  setSortOrder,
+  sortOrder,
+}) => {
   const navigate = useNavigate();
 
   const repositoryNodes = repositories
@@ -12,24 +111,36 @@ export const RepositoryListContainer = ({ repositories }) => {
     : [];
 
   return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) => {
-        return (
-          <Pressable onPress={() => navigate(`/${item.id}`)}>
-            <RepositoryItem repository={item} />
-          </Pressable>
-        );
-      }}
-    />
+    <PaperProvider>
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        ListHeaderComponent={() => (
+          <ListSortSelect setSortOrder={setSortOrder} sortOrder={sortOrder} />
+        )}
+        renderItem={({ item }) => {
+          return (
+            <Pressable onPress={() => navigate(`/${item.id}`)}>
+              <RepositoryItem repository={item} />
+            </Pressable>
+          );
+        }}
+      />
+    </PaperProvider>
   );
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [sortOrder, setSortOrder] = useState('LATEST');
+  const { repositories } = useRepositories(sortOrder);
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <RepositoryListContainer
+      repositories={repositories}
+      sortOrder={sortOrder}
+      setSortOrder={setSortOrder}
+    />
+  );
 };
 
 export default RepositoryList;
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,9 +1,31 @@
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
+const useRepositories = (sortOrder) => {
+  let orderBy;
+  let orderDirection;
+
+  switch (sortOrder) {
+    case 'HIGHEST_RATED':
+      orderBy = 'RATING_AVERAGE';
+      orderDirection = 'DESC';
+      break;
+    case 'LOWEST_RATED':
+      orderBy = 'RATING_AVERAGE';
+      orderDirection = 'ASC';
+      break;
+    case 'LATEST':
+    default:
+      orderBy = 'CREATED_AT';
+      break;
+  }
+
   // eslint-disable-next-line no-unused-vars
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    variables: {
+      orderBy,
+      orderDirection,
+    },
     fetchPolicy: 'cache-and-network',
   });
 
